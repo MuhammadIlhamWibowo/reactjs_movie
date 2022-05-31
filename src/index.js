f57905107f88e635373408f514c56cbf7cd46aa8
MuhammadIlhamWibowo <muhammad.ilham.wibowo@gmail.com>
2022-05-31 15:05:01 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+// http://www.omdbapi.com/?i=tt3896198&apikey=350d923a
+
+ReactDOM.render(<App />, document.getElementById("root"));
